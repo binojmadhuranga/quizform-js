82e2d9fb10bb344c033a62494723967488543def
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-05-14 19:06:19 +0530

    update ome features.

--- index.js
+++ index.js
@@ -37,12 +37,49 @@ window.onload = function () {
         } else {
             clearInterval(timer);
             countdownElement.innerText = "0";
-             window.location.href = "quiz02.html";
+            window.location.href = "quiz02.html";
             alert("Time's up! Moving to the next quiz.");
 
         }
     }, 1000);
 }
+
+
+
+let quiz01 = () => {
+
+    let score ;
+    const answer1 = document.getElementById("option1").checked;
+    const answer2 = document.getElementById("option2").checked;
+    const answer3 = document.getElementById("option3").checked;
+    const answer4 = document.getElementById("option4").checked;
+   
+    if (answer1) {
+        alert("answer Submitted!");
+        localStorage.setItem("score", 1);
+        window.location.href = "quiz02.html";
+    }
+    else if (answer2) {
+        alert("answer Submitted!");
+        localStorage.setItem("score", 0);
+        window.location.href = "quiz02.html";
+    }
+    else if (answer3) {
+        alert("answer Submitted!");
+        localStorage.setItem("score", 0);
+        window.location.href = "quiz02.html";
+    }
+    else if (answer4) {
+        alert("answer Submitted!");
+        localStorage.setItem("score", 0);
+        window.location.href = "quiz02.html";
+    }
+    else {
+        alert("Please select an answer");
+    }
+
+
+};
 
 
 
